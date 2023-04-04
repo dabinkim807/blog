5cec218aa54eeaba80b3cf818b640eb98d2fdf2f
Dana <dabinkim807@gmail.com>
2023-04-04 17:02:03 -0400

    add put routes to server and fix bug in post comments route

--- server/server.js
+++ server/server.js
@@ -68,8 +68,8 @@ app.post('/api/comments', async (req, res) => {
         const returnObj = {
             comment_id: result.rows[0].comment_id,
             date: result.rows[0].date,
-            comment: req.body.title,
-            comment_author: req.body.author,
+            comment: req.body.comment,
+            comment_author: req.body.comment_author,
             post_id: findPost.rows[0].post_id
         }
         return res.status(200).json(returnObj);
@@ -90,39 +90,45 @@ app.post('/api/users', async (req, res) => {
     }
 });
 
-// app.delete('/api/students/:studentId', async (req, res) => {
-//     try {
-//         const studentId = req.params.studentId;
-//         await db.query('DELETE FROM students WHERE id=$1', [studentId]);
-//         console.log("From the delete request-url", studentId);
-//         res.status(200).end();
-//     } catch (e) {
-//         console.log(e);
-//         return res.status(400).json({ e });
-
-//     }
-// });
-
-// app.put('/api/students/:studentId', async (req, res) =>{
-//     //console.log(req.params);
-//     //This will be the id that I want to find in the DB - the student to be updated
-//     const studentId = req.params.studentId
-//     const updatedStudent = { id: req.body.id, firstname: req.body.firstname, lastname: req.body.lastname, iscurrent: req.body.is_current}
-//     console.log("In the server from the url - the student id", studentId);
-//     console.log("In the server, from the react - the student to be edited", updatedStudent);
-//     // UPDATE students SET lastname = "something" WHERE id="16";
-//     const query = `UPDATE students SET firstname=$1, lastname=$2, is_current=$3 WHERE id=${studentId} RETURNING *`;
-//     const values = [updatedStudent.firstname, updatedStudent.lastname, updatedStudent.iscurrent];
-//     try {
-//       const updated = await db.query(query, values);
-//       console.log(updated.rows[0]);
-//       res.send(updated.rows[0]);
-  
-//     }catch(e){
-//       console.log(e);
-//       return res.status(400).json({e})
-//     }
-//   })
+app.put('/api/posts/:postID', async (req, res) => {
+    const post_id = parseInt(req.params.postID);
+	try {
+		await db.query(
+			"UPDATE posts SET title = $1, author = $2, post_body = $3 WHERE post_id = $4 RETURNING *", 
+			[req.body.title, req.body.author, req.body.post_body, post_id]
+		);
+	} catch(e) {
+        console.log(e);
+		return res.status(400).send(String(e));
+	}
+	return res.end();
+});
+app.put('/api/comments/:commentID', async (req, res) => {
+    const comment_id = parseInt(req.params.commentID);
+	try {
+		await db.query(
+			"UPDATE comments SET comment = $1, comment_author = $2 WHERE comment_id = $3 RETURNING *", 
+			[req.body.comment, req.body.comment_author, comment_id]
+		);
+	} catch(e) {
+        console.log(e);
+		return res.status(400).send(String(e));
+	}
+	return res.end();
+});
+app.put('/api/users/:userID', async (req, res) => {
+    const user_id = parseInt(req.params.userID);
+	try {
+		await db.query(
+			"UPDATE users SET name = $1 WHERE user_id = $2 RETURNING *", 
+			[req.body.name, user_id]
+		);
+	} catch(e) {
+        console.log(e);
+		return res.status(400).send(String(e));
+	}
+	return res.end();
+});
 
 app.listen(PORT, () => {
     console.log(`Hello, my server is listening on ${PORT}`);
