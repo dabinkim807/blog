9726f648bf93a6c6de2273a3be3138c0bed1c9ab
Dana <dabinkim807@gmail.com>
2023-04-07 14:16:51 -0400

    delete unused code in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,12 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import MyNavBar from './components/Navbar'
-import ListStudents from './components/ListStudents'
+
 
 
 function App() {
 
   return (
     <div className="App">
-      <MyNavBar />
-      <ListStudents />
 
     </div>
   )
